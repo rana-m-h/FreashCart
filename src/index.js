9ed768a42af902f1e6207a2b7cf9d9b8c45a4e8d
rana-m-h <rana29638@gmail.com>
2024-03-01 22:35:40 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './index.css';
-import App from './App';
+import App from './App.jsx';
+
 import reportWebVitals from './reportWebVitals';
+import UserContextProvider from './Context/Usercontext.js';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import CartContextProvider from './Context/CartContext.js';
+import ListContextProvider from './Context/LIstContext.js';
+import PasswordContextProvider from './Context/Password.js';
+
+
+
+
+
+let queryClient = new QueryClient()
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+<ListContextProvider>
+<CartContextProvider>
+      <UserContextProvider>
+            <QueryClientProvider client={queryClient}>
+                  <App />
+            </QueryClientProvider>
+
+      </UserContextProvider>
+      </CartContextProvider>
+      </ListContextProvider>
+   
 );
 
 // If you want to start measuring performance in your app, pass a function
